1921852c3011beeffa025df2ec2c21c26c844c81
Denys <165385842+DoggyDoggyDoggy@users.noreply.github.com>
2025-07-18 11:00:07 +1200

    feat: Rename GetCityHistoryById to GetCityHistoryByIdUseCase
    This commit renames the use case `GetCityHistoryById` to `GetCityHistoryByIdUseCase` for better clarity and consistency in naming conventions.

--- app/src/main/java/denys/diomaxius/newzealandguide/domain/usecase/GetCityHistoryByIdUseCase.kt
+++ app/src/main/java/denys/diomaxius/newzealandguide/domain/usecase/GetCityHistoryByIdUseCase.kt
@@ -4,7 +4,7 @@ import denys.diomaxius.newzealandguide.domain.model.city.CityHistory
 import denys.diomaxius.newzealandguide.domain.repository.CityRepository
 import javax.inject.Inject
 
-class GetCityHistoryById @Inject constructor(
+class GetCityHistoryByIdUseCase @Inject constructor(
     private val cityRepository: CityRepository,
 ) {
     suspend operator fun invoke(cityId: String): CityHistory =
