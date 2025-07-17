f1eb1e101d613214a899d859286ea43d8dc8bfb2
Denys <165385842+DoggyDoggyDoggy@users.noreply.github.com>
2025-07-17 18:59:07 +1200

    feat: Implement city detail screen and data loading
    This commit introduces the `CityScreen` to display details of a specific city and implements the data loading logic in `CityScreenViewModel`.
    
    Key changes:
    - **CityScreen.kt:**
        - Created `CityScreen` composable which observes city data from `CityScreenViewModel`.
        - Displays "Loading", "Error", or the city name based on the `UiState`.
        - Added a simple `Content` composable to display the city name.
    - **CityScreenViewModel.kt:**
        - Retrieves `cityId` from `SavedStateHandle`.
        - Uses `GetCityByIdUseCase` to fetch city data within `viewModelScope`.
        - Manages the state of city loading using `MutableStateFlow<UiState<City>>` and exposes it as `StateFlow`.
        - Handles success and error states for data fetching.
    - **AllCitiesScreen.kt:**
        - Updated navigation from `AllCitiesScreen` to navigate to `NavScreen.City` instead of `NavScreen.CityPlaces` when a city is selected, passing the `city.id`.

--- app/src/main/java/denys/diomaxius/newzealandguide/ui/city/CityScreen.kt
+++ app/src/main/java/denys/diomaxius/newzealandguide/ui/city/CityScreen.kt
@@ -1,11 +1,35 @@
 package denys.diomaxius.newzealandguide.ui.city
 
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.hilt.navigation.compose.hiltViewModel
+import denys.diomaxius.newzealandguide.domain.model.city.City
+import denys.diomaxius.newzealandguide.navigation.LocalNavController
+import denys.diomaxius.newzealandguide.ui.common.UiState
 
 @Composable
 fun CityScreen(
     viewModel: CityScreenViewModel = hiltViewModel()
 ) {
+    val city by viewModel.city.collectAsState()
+    val navHostController = LocalNavController.current
 
+    when(city) {
+        is UiState.Loading -> {
+            Text(text = "Loading")
+        }
+        is UiState.Success -> Content(
+          city = (city as UiState.Success<City>).data
+        )
+        is UiState.Error -> {
+            Text(text = "Error")
+        }
+    }
+}
+
+@Composable
+fun Content(city: City) {
+    Text(text = city.name)
 }
