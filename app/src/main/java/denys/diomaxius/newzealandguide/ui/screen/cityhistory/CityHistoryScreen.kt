1dc8e3ee253ca96547d0fb179627818748b3c6af
Denys <165385842+DoggyDoggyDoggy@users.noreply.github.com>
2025-07-18 17:54:13 +1200

    feat: Add TopBar to CityHistoryScreen
    This commit introduces a TopBar to the `CityHistoryScreen`.
    
    Key changes:
    - Wrapped the `Content` of `CityHistoryScreen` within a `Scaffold`.
    - Added a `TopBar` to the `Scaffold` with the title "History".
    - Included a `PopBackArrowButton` in the `TopBar` to enable navigation back to the previous screen.
    - Renamed `cityHistory` StateFlow in `CityHistoryScreenViewModel` to `cityHistoryUiState` for clarity.
    - Adjusted the `Content` composable to accept a `Modifier` for padding applied by the `Scaffold`.

--- app/src/main/java/denys/diomaxius/newzealandguide/ui/screen/cityhistory/CityHistoryScreen.kt
+++ app/src/main/java/denys/diomaxius/newzealandguide/ui/screen/cityhistory/CityHistoryScreen.kt
@@ -5,6 +5,7 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
@@ -14,25 +15,46 @@ import androidx.compose.runtime.getValue
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import denys.diomaxius.newzealandguide.domain.model.city.CityHistory
+import denys.diomaxius.newzealandguide.navigation.LocalNavController
 import denys.diomaxius.newzealandguide.ui.common.components.UiStateHandler
+import denys.diomaxius.newzealandguide.ui.common.components.topbar.PopBackArrowButton
+import denys.diomaxius.newzealandguide.ui.common.components.topbar.TopBar
 
 @Composable
 fun CityHistoryScreen(
     viewModel: CityHistoryScreenViewModel = hiltViewModel(),
 ) {
-    val cityHistory by viewModel.cityHistory.collectAsState()
+    val cityHistoryUiState by viewModel.cityHistoryUiState.collectAsState()
+    val navHostController = LocalNavController.current
 
-    UiStateHandler(cityHistory) {
-        Content(it)
+    UiStateHandler(cityHistoryUiState) { cityHistory ->
+        Scaffold(
+            topBar = {
+                TopBar(
+                    text = "History",
+                    navigationButton = {
+                        PopBackArrowButton {
+                            navHostController.popBackStack()
+                        }
+                    }
+                )
+            }
+        ) { innerPadding ->
+            Content(
+                modifier = Modifier.padding(innerPadding),
+                cityHistory = cityHistory
+            )
+        }
     }
 }
 
 @Composable
 fun Content(
+    modifier: Modifier = Modifier,
     cityHistory: CityHistory,
 ) {
     Column(
-        modifier = Modifier
+        modifier = modifier
             .fillMaxSize()
             .padding(8.dp)
     ) {
