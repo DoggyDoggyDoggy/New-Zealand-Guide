911c06a626ae67664b34b295a42b3ab4b42ae0ef
Denys <165385842+DoggyDoggyDoggy@users.noreply.github.com>
2025-07-20 11:57:18 +1200

    Refactor: Update comments in WeatherEntity
    The comments in `WeatherEntity.kt` have been updated to reflect considerations about renaming `dt_txt` and its immutability.

--- app/src/main/java/denys/diomaxius/newzealandguide/data/model/WeatherEntity.kt
+++ app/src/main/java/denys/diomaxius/newzealandguide/data/model/WeatherEntity.kt
@@ -1,8 +1,12 @@
 package denys.diomaxius.newzealandguide.data.model
 
+// Maybe rename dt_txt to dtTxt
+// dt_txt in firestore
+// don't want to make in mutable
+
 data class WeatherEntity(
     val temperature: Double = 0.0,
     val description: String = "",
     val iconUrl: String = "",
-    val dt_txt: String = ""
+    val dt_txt: String = "",
 )
