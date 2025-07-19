b91aef9152c82d59f2c09e08ec27177e8fb792bc
Denys <165385842+DoggyDoggyDoggy@users.noreply.github.com>
2025-07-20 11:29:30 +1200

    feat: Implement weather forecast fetching in ViewModel
    This commit introduces the logic to fetch weather forecast data in `WeatherForecastFiveDaysViewModel`.
    
    Key changes:
    - Injected `SavedStateHandle` to retrieve `cityId`.
    - Added a `MutableStateFlow` named `_weatherForecastUiState` to hold the UI state (Loading, Success, Error) of the weather forecast data, typed as `UiState<List<Weather>>`.
    - Exposed an immutable `StateFlow` named `weatherForecastUiState`.
    - Implemented `loadWeatherForecast()` function which is called in `init` block:
      - It launches a coroutine in `viewModelScope`.
      - Calls `getWeatherByCityIdUseCase` with the `cityId`.
      - Updates `_weatherForecastUiState` with `UiState.Success` containing the weather data or `UiState.Error` if an exception occurs.

--- app/src/main/java/denys/diomaxius/newzealandguide/ui/common/components/weather/WeatherForecastFiveDaysViewModel.kt
+++ app/src/main/java/denys/diomaxius/newzealandguide/ui/common/components/weather/WeatherForecastFiveDaysViewModel.kt
@@ -1,13 +1,36 @@
 package denys.diomaxius.newzealandguide.ui.common.components.weather
 
+import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
+import denys.diomaxius.newzealandguide.domain.model.weather.Weather
 import denys.diomaxius.newzealandguide.domain.usecase.GetWeatherByCityIdUseCase
+import denys.diomaxius.newzealandguide.ui.common.UiState
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class WeatherForecastFiveDaysViewModel @Inject constructor(
-    private val getWeatherByCityIdUseCase: GetWeatherByCityIdUseCase
-): ViewModel() {
+    private val getWeatherByCityIdUseCase: GetWeatherByCityIdUseCase,
+    savedStateHandle: SavedStateHandle,
+) : ViewModel() {
+    private val _weatherForecastUiState = MutableStateFlow<UiState<List<Weather>>>(UiState.Loading)
+    val weatherForecastUiState = _weatherForecastUiState.asStateFlow()
 
+    private val cityId: String = checkNotNull(savedStateHandle["cityId"])
+
+    init {
+        loadWeatherForecast()
+    }
+
+    private fun loadWeatherForecast() = viewModelScope.launch {
+        _weatherForecastUiState.value = try {
+            UiState.Success(getWeatherByCityIdUseCase(cityId))
+        } catch (e: Exception) {
+            UiState.Error(e)
+        }
+    }
 }
