366f3c02c4dd798fed0d9f5a334e09fa353308f8
Denys <165385842+DoggyDoggyDoggy@users.noreply.github.com>
2025-07-20 17:29:26 +1200

    feat: Implement fetching weather icons
    This commit introduces the functionality to fetch weather icons.
    
    - **WeatherRepository:**
        - Added `getWeatherIcons()` function to retrieve a list of `WeatherIcon` objects from Firestore.
    - **WeatherRepositoryImpl:**
        - Implemented `getWeatherIcons()` to fetch data from the "weatherIcons" collection in Firestore.
    - **GetWeatherIconUseCase:**
        - Created a new use case to provide weather icons from the repository.
    - **WeatherForecastFiveDaysViewModel:**
        - Injected `GetWeatherIconUseCase` to enable fetching weather icons.

--- app/src/main/java/denys/diomaxius/newzealandguide/domain/repository/WeatherRepository.kt
+++ app/src/main/java/denys/diomaxius/newzealandguide/domain/repository/WeatherRepository.kt
@@ -1,7 +1,9 @@
 package denys.diomaxius.newzealandguide.domain.repository
 
 import denys.diomaxius.newzealandguide.domain.model.weather.Weather
+import denys.diomaxius.newzealandguide.domain.model.weather.WeatherIcon
 
 interface WeatherRepository {
     suspend fun getWeatherByCityId(cityId: String): List<Weather>
+    suspend fun getWeatherIcons(): List<WeatherIcon>
 }
