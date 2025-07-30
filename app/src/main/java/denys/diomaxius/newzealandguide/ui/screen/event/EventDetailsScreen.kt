8ae2eb137eed310467dd11e4e6ddf275f4facbd9
Denys <165385842+DoggyDoggyDoggy@users.noreply.github.com>
2025-07-30 22:51:50 +1200

    feat: Implement event details fetching and display
    This commit implements the functionality to fetch and display event details.
    
    - **EventDetailsScreenViewModel:**
        - Injected `GetEventInCityByIdUseCase`.
        - Retrieves `eventId` and `cityId` from `SavedStateHandle`.
        - Added a `MutableStateFlow` named `_uiState` to hold the UI state (`UiState<Event>`) for the event details.
        - Exposed an immutable `StateFlow` named `uiState`.
        - Implemented `loadEvent()` function, called in `init`, to fetch event details using `getEventInCityByIdUseCase` and update `_uiState` with `UiState.Success` or `UiState.Error`.
    - **EventDetailsScreen:**
        - Collects `uiState` from the ViewModel.
        - Uses `UiStateHandler` to manage loading and error states.
        - Added a `Content` composable to display the event name when data is successfully loaded.

--- app/src/main/java/denys/diomaxius/newzealandguide/ui/screen/event/EventDetailsScreen.kt
+++ app/src/main/java/denys/diomaxius/newzealandguide/ui/screen/event/EventDetailsScreen.kt
@@ -2,11 +2,26 @@ package denys.diomaxius.newzealandguide.ui.screen.event
 
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.compose.runtime.getValue
+import denys.diomaxius.newzealandguide.domain.model.event.Event
+import denys.diomaxius.newzealandguide.ui.common.uistate.UiStateHandler
 
 @Composable
 fun EventDetailsScreen(
     viewModel: EventDetailsScreenViewModel = hiltViewModel()
 ) {
-    Text("Event Detail")
+    val uiState by viewModel.uiState.collectAsState()
+
+    UiStateHandler(uiState) { event ->
+        Content(event = event)
+    }
+}
+
+@Composable
+fun Content(event: Event) {
+    Text(
+        text = event.name
+    )
 }
