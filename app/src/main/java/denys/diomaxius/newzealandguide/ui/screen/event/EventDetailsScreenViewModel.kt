7d3b5c1a535b328b1da3cacde1810400e6b5b533
Denys <165385842+DoggyDoggyDoggy@users.noreply.github.com>
2025-07-30 22:51:50 +1200

    feat: Implement event details fetching and display
    This commit implements the functionality to fetch and display event details.
    
    - **EventDetailsScreenViewModel:**
        - Injected `GetEventInCityByIdUseCase`.
        - Retrieves `eventId` and `cityId` from `SavedStateHandle`.
        - Added a `MutableStateFlow` named `_uiState` to hold the UI state (`UiState<Event>`) for the event details.
        - Exposed an immutable `StateFlow` named `uiState`.
        - Implemented `loadEvent()` function, called in `init`, to fetch event details using `getEventInCityByIdUseCase` and update `_uiState` with `UiState.Success` or `UiState.Error`.
    - **EventDetailsScreen:**
        - Collects `uiState` from the ViewModel.
        - Uses `UiStateHandler` to manage loading and error states.
        - Added a `Content` composable to display the event name when data is successfully loaded.

--- app/src/main/java/denys/diomaxius/newzealandguide/ui/screen/event/EventDetailsScreenViewModel.kt
+++ app/src/main/java/denys/diomaxius/newzealandguide/ui/screen/event/EventDetailsScreenViewModel.kt
@@ -2,13 +2,37 @@ package denys.diomaxius.newzealandguide.ui.screen.event
 
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
+import denys.diomaxius.newzealandguide.domain.model.event.Event
+import denys.diomaxius.newzealandguide.domain.usecase.event.GetEventInCityByIdUseCase
+import denys.diomaxius.newzealandguide.ui.common.uistate.UiState
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class EventDetailsScreenViewModel @Inject constructor(
+    private val getEventInCityByIdUseCase: GetEventInCityByIdUseCase,
     savedStateHandle: SavedStateHandle
 ) : ViewModel() {
+    private val _uiState =
+        MutableStateFlow<UiState<Event>>(UiState.Loading)
+    val uiState = _uiState.asStateFlow()
+
     private val eventId: String = checkNotNull(savedStateHandle["eventId"])
     private val cityId: String = checkNotNull(savedStateHandle["cityId"])
+
+    init {
+        loadEvent()
+    }
+
+    private fun loadEvent() = viewModelScope.launch {
+        _uiState.value = try {
+            UiState.Success(getEventInCityByIdUseCase(cityId = cityId, eventId = eventId))
+        } catch (e: Exception) {
+            UiState.Error(e)
+        }
+    }
 }
