67cc558aaf67e79142231521d4392fae5653bd42
Denys <165385842+DoggyDoggyDoggy@users.noreply.github.com>
2025-07-31 22:34:03 +1200

    feat: Enhance EventDetailsScreen with UI elements
    This commit significantly enhances the `EventDetailsScreen` by:
    
    - **Adding a Scaffold:** Implemented a Scaffold with a TopBar that includes a title ("Event Details") and a pop-back arrow button for navigation.
    - **Introducing Content Structure:** The `Content` composable now displays event details within a `Card`.
    - **Displaying Event Information:**
        - Event name is displayed with bold font and larger size.
        - Event image is loaded using `AsyncImage` and displayed with rounded corners.
        - `EventDescription` composable is used to show the event description.
        - `EventAddress` composable is used to show the event address with a location icon.
    - **Layout and Styling:**
        - Added padding and spacers for better visual separation and layout.
        - Implemented vertical scrolling for content that might exceed screen height.
    - **New Composable Components:**
        - **EventAddress:** Created a new composable (`EventAddress.kt`) to display the event address, featuring a location icon and formatted text.
        - **EventDescription:** Created a new composable (`EventDescription.kt`) to display the event description with a "Description:" label and formatted text.

--- app/src/main/java/denys/diomaxius/newzealandguide/ui/screen/event/EventDetailsScreen.kt
+++ app/src/main/java/denys/diomaxius/newzealandguide/ui/screen/event/EventDetailsScreen.kt
@@ -1,27 +1,109 @@
 package denys.diomaxius.newzealandguide.ui.screen.event
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Card
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import coil3.compose.AsyncImage
 import denys.diomaxius.newzealandguide.domain.model.event.Event
+import denys.diomaxius.newzealandguide.navigation.LocalNavController
+import denys.diomaxius.newzealandguide.ui.common.components.topbar.PopBackArrowButton
+import denys.diomaxius.newzealandguide.ui.common.components.topbar.TopBar
 import denys.diomaxius.newzealandguide.ui.common.uistate.UiStateHandler
+import denys.diomaxius.newzealandguide.ui.screen.event.components.EventAddress
+import denys.diomaxius.newzealandguide.ui.screen.event.components.EventDescription
 
 @Composable
 fun EventDetailsScreen(
-    viewModel: EventDetailsScreenViewModel = hiltViewModel()
+    viewModel: EventDetailsScreenViewModel = hiltViewModel(),
 ) {
     val uiState by viewModel.uiState.collectAsState()
+    val navHostController = LocalNavController.current
 
     UiStateHandler(uiState) { event ->
-        Content(event = event)
+        Scaffold(
+            topBar = {
+                TopBar(
+                    text = "Event Details",
+                    navigationButton = {
+                        PopBackArrowButton {
+                            navHostController.popBackStack()
+                        }
+                    }
+                )
+            }
+        ) { innerPadding ->
+            Content(
+                modifier = Modifier.padding(innerPadding),
+                event = event
+            )
+        }
     }
 }
 
 @Composable
-fun Content(event: Event) {
-    Text(
-        text = event.name
-    )
+fun Content(
+    modifier: Modifier,
+    event: Event,
+) {
+    Card(
+        modifier = modifier
+            .padding(top = 12.dp)
+            .padding(horizontal = 12.dp)
+    ) {
+        Column(
+            modifier = Modifier
+                //.padding(vertical = 12.dp)
+                .padding(horizontal = 12.dp)
+                .verticalScroll(rememberScrollState())
+        ) {
+            Spacer(modifier = Modifier.height(12.dp))
+
+            Text(
+                text = event.name,
+                fontWeight = FontWeight.Bold,
+                fontSize = 24.sp
+            )
+
+            Spacer(modifier = Modifier.height(6.dp))
+
+            AsyncImage(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .clip(RoundedCornerShape(8.dp)),
+                model = event.imageUrl,
+                contentScale = ContentScale.FillWidth,
+                contentDescription = "Image"
+            )
+
+            Spacer(modifier = Modifier.height(6.dp))
+
+            EventDescription(event.description)
+
+            Spacer(modifier = Modifier.height(12.dp))
+
+            EventAddress(event.address)
+
+            Spacer(modifier = Modifier.height(4.dp))
+
+            //EventDates(event)
+        }
+    }
 }
