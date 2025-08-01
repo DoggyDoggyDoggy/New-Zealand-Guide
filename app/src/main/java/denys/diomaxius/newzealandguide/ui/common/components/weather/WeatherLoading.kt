46cfae8117f9edbcad4a16f3e5e331db58e4dfab
Denys <165385842+DoggyDoggyDoggy@users.noreply.github.com>
2025-08-01 17:12:29 +1200

    Refactor: Remove unused import in WeatherLoading
    The `Column` import from `androidx.compose.foundation.layout.Column` has been removed from `WeatherLoading.kt` as it was not being used.

--- app/src/main/java/denys/diomaxius/newzealandguide/ui/common/components/weather/WeatherLoading.kt
+++ app/src/main/java/denys/diomaxius/newzealandguide/ui/common/components/weather/WeatherLoading.kt
@@ -2,7 +2,6 @@ package denys.diomaxius.newzealandguide.ui.common.components.weather
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
