0bde759b24264d371c0d175fcc9e5f06ca221ce7
Denys <165385842+DoggyDoggyDoggy@users.noreply.github.com>
2025-08-10 14:58:24 +1200

    feat: Implement data layer for New Zealand History
    This commit introduces the data layer components necessary for fetching New Zealand history information from Firestore.
    
    Key additions:
    - **NewZealandHistoryEntity:** Data class representing the structure of New Zealand history data in Firestore.
    - **NewZealandHistoryMapper:** A mapper function `toDomain()` to convert `NewZealandHistoryEntity` to the domain model `NewZealandHistory`.
    - **NewZealandHistoryRepository:** Interface defining the contract for retrieving New Zealand history.
    - **NewZealandHistoryRepositoryImpl:** Implementation of `NewZealandHistoryRepository` that fetches data from the "nzHistory" collection in Firestore.
    - **GetNewZealandHistoryUseCase:** A use case class that utilizes `NewZealandHistoryRepository` to provide New Zealand history data to the presentation layer.

---
+++ app/src/main/java/denys/diomaxius/newzealandguide/data/model/nzhistory/NewZealandHistoryEntity.kt
@@ -0,0 +1,6 @@
+package denys.diomaxius.newzealandguide.data.model.nzhistory
+
+data class NewZealandHistoryEntity(
+    val paragraphs: List<String> = emptyList(),
+    val paragraphsTitles: List<String> = emptyList()
+)
