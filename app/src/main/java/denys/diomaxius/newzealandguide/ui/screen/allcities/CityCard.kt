372e127995e2066df6347a1ac1da9493a8dd0439
Denys <165385842+DoggyDoggyDoggy@users.noreply.github.com>
2025-08-18 09:23:24 +1200

    Refactor: Extract CityCard to separate file
    The `CityCard` composable has been moved from `AllCitiesScreen.kt` to its own file, `CityCard.kt`, within the same package. This improves code organization and modularity.

---
+++ app/src/main/java/denys/diomaxius/newzealandguide/ui/screen/allcities/CityCard.kt
@@ -0,0 +1,43 @@
+package denys.diomaxius.newzealandguide.ui.screen.allcities
+
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Card
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.unit.dp
+import coil3.compose.AsyncImage
+import denys.diomaxius.newzealandguide.domain.model.city.City
+import denys.diomaxius.newzealandguide.ui.common.components.TextOverlay
+
+@Composable
+fun CityCard(
+    city: City,
+    navigateToCity: () -> Unit,
+) {
+    Card(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(12.dp)
+            .clickable {
+                navigateToCity()
+            }
+    ) {
+        Box(
+            contentAlignment = Alignment.BottomStart
+        ) {
+            AsyncImage(
+                modifier = Modifier.fillMaxWidth(),
+                model = city.photos.first(),
+                contentDescription = city.name,
+                contentScale = ContentScale.FillWidth
+            )
+
+            TextOverlay(text = city.name)
+        }
+    }
+}
