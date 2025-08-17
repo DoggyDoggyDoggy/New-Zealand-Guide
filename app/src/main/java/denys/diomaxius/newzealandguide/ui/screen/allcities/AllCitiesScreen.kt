7afb5068e798fb487d2197e1693bd3589c0dd766
Denys <165385842+DoggyDoggyDoggy@users.noreply.github.com>
2025-08-18 09:23:24 +1200

    Refactor: Extract CityCard to separate file
    The `CityCard` composable has been moved from `AllCitiesScreen.kt` to its own file, `CityCard.kt`, within the same package. This improves code organization and modularity.

--- app/src/main/java/denys/diomaxius/newzealandguide/ui/screen/allcities/AllCitiesScreen.kt
+++ app/src/main/java/denys/diomaxius/newzealandguide/ui/screen/allcities/AllCitiesScreen.kt
@@ -1,29 +1,20 @@
 package denys.diomaxius.newzealandguide.ui.screen.allcities
 
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material3.Card
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavHostController
-import coil3.compose.AsyncImage
 import denys.diomaxius.newzealandguide.R
 import denys.diomaxius.newzealandguide.domain.model.city.City
 import denys.diomaxius.newzealandguide.navigation.LocalNavController
 import denys.diomaxius.newzealandguide.navigation.NavScreen
-import denys.diomaxius.newzealandguide.ui.common.components.TextOverlay
 import denys.diomaxius.newzealandguide.ui.common.components.loadingscreen.ScreenLoading
 import denys.diomaxius.newzealandguide.ui.common.uistate.UiStateHandler
 import denys.diomaxius.newzealandguide.ui.common.components.topbar.PopBackArrowButton
@@ -81,34 +72,6 @@ fun Content(
                     }
                 }
             )
-        }
-    }
-}
-
-@Composable
-fun CityCard(
-    city: City,
-    navigateToCity: () -> Unit,
-) {
-    Card(
-        modifier = Modifier
-            .fillMaxWidth()
-            .padding(12.dp)
-            .clickable {
-                navigateToCity()
-            }
-    ) {
-        Box(
-            contentAlignment = Alignment.BottomStart
-        ) {
-            AsyncImage(
-                modifier = Modifier.fillMaxWidth(),
-                model = city.photos.first(),
-                contentDescription = city.name,
-                contentScale = ContentScale.FillWidth
-            )
-
-            TextOverlay(text = city.name)
         }
     }
 }
