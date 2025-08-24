85d5fd38607d6921451fdb1afc2b3545161c8910
Denys <165385842+DoggyDoggyDoggy@users.noreply.github.com>
2025-08-24 12:41:29 +1200

    feat: Add favorite icon to CityCard and Preview
    This commit introduces a "Favorite" icon to the `CityCard` composable and adds a `@Preview` function for it.
    
    - **CityCard.kt:**
        - Added an `Icon` composable to display a favorite icon (defaulting to `FavoriteBorder`).
        - The icon is positioned at the top-end of the card with padding and a specific size.
        - The icon is tinted white for better visibility against the image.
    - **CityCardPreview:**
        - Created a new `@Preview` composable function named `CityCardPreview`.
        - This preview displays a sample `CityCard` with predefined city data, allowing for easier UI development and testing of the `CityCard` component.
    - **.idea/deploymentTargetSelector.xml:** Updated with a run configuration for `CityCardPreview`.

--- app/src/main/java/denys/diomaxius/newzealandguide/ui/screen/allcities/CityCard.kt
+++ app/src/main/java/denys/diomaxius/newzealandguide/ui/screen/allcities/CityCard.kt
@@ -4,11 +4,17 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.FavoriteBorder
 import androidx.compose.material3.Card
+import androidx.compose.material3.Icon
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import coil3.compose.AsyncImage
 import denys.diomaxius.newzealandguide.domain.model.city.City
@@ -30,6 +36,7 @@ fun CityCard(
         Box(
             contentAlignment = Alignment.BottomStart
         ) {
+
             AsyncImage(
                 modifier = Modifier.fillMaxWidth(),
                 model = city.photos.first(),
@@ -37,7 +44,31 @@ fun CityCard(
                 contentScale = ContentScale.FillWidth
             )
 
+
+            Icon(
+                imageVector = Icons.Default.FavoriteBorder,
+                contentDescription = "Favorite",
+                modifier = Modifier
+                    .align(Alignment.TopEnd)
+                    .padding(2.dp)
+                    .size(58.dp),
+                tint = Color.White
+            )
+
             TextOverlay(text = city.name)
         }
     }
+}
+
+@Preview
+@Composable
+fun CityCardPreview() {
+    CityCard(
+        city = City(
+            id = "2",
+            name = "Auckland",
+            photos = listOf("https://res.cloudinary.com/dpeak0qy7/image/upload/v1754316593/New_Project_3_lgsztg.png")
+        ),
+        navigateToCity = {}
+    )
 }
