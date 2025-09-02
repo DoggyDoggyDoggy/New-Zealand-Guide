fa7d66d9c39eff7b4b8b09022c79117d9653580e
Denys <165385842+DoggyDoggyDoggy@users.noreply.github.com>
2025-09-02 12:48:53 +1200

    feat: Add empty state for favorite cities
    This commit introduces an `EmptyFavoriteScreen` composable that is displayed when the user navigates to the favorites tab and has no favorite cities saved.
    
    - **EmptyFavoriteScreen.kt:**
        - Created a new composable that displays a large favorite icon and a message "Add your favorite cities" when the favorite list is empty.
    - **AllCitiesScreen.kt:**
        - Modified `CitiesContent` to show `EmptyFavoriteScreen` when `showFavorite` is true and the `cities` list is empty.

---
+++ app/src/main/java/denys/diomaxius/newzealandguide/ui/screen/allcities/components/EmptyFavoriteScreen.kt
@@ -0,0 +1,43 @@
+package denys.diomaxius.newzealandguide.ui.screen.allcities.components
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+@Composable
+fun EmptyFavoriteScreen() {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Icon(
+            modifier = Modifier
+                .size(128.dp),
+            imageVector = Icons.Default.Favorite,
+            contentDescription = "Favorite",
+            tint = Color.Gray
+        )
+
+        Spacer(modifier = Modifier.height(12.dp))
+
+        Text(
+            text = "Add your favorite cities",
+            fontSize = 24.sp,
+            color = Color.Gray
+        )
+    }
+}
