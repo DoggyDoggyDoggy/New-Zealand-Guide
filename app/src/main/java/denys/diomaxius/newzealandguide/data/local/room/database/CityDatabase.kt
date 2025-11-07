f22e4e39e56457d476b0239904c00175dc65c33d
Denys <165385842+DoggyDoggyDoggy@users.noreply.github.com>
2025-11-07 19:33:21 +1300

    feat: Add TypeConverters to Room database
    Adds the `@TypeConverters` annotation to the `CityDatabase`, registering the `Converters` class. This enables Room to handle custom data types that are not natively supported.

--- app/src/main/java/denys/diomaxius/newzealandguide/data/local/room/database/CityDatabase.kt
+++ app/src/main/java/denys/diomaxius/newzealandguide/data/local/room/database/CityDatabase.kt
@@ -2,6 +2,8 @@ package denys.diomaxius.newzealandguide.data.local.room.database
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
+import denys.diomaxius.newzealandguide.data.local.room.converter.Converters
 import denys.diomaxius.newzealandguide.data.local.room.dao.CityDao
 import denys.diomaxius.newzealandguide.data.local.room.model.city.CityEntity
 
@@ -11,6 +13,7 @@ import denys.diomaxius.newzealandguide.data.local.room.model.city.CityEntity
     ],
     version = 1
 )
+@TypeConverters(Converters::class)
 abstract class CityDatabase: RoomDatabase() {
     companion object {
         const val DATABASE_NAME = "city_db"
