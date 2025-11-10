798f632fa882540a6fc4087f4fe407aef20b980e
Denys <165385842+DoggyDoggyDoggy@users.noreply.github.com>
2025-11-10 13:48:31 +1300

    feat: Add CityCard and TextOverlay composables
    Introduces two new composables for displaying city information.
    
    This commit adds:
    - `TextOverlay`: A reusable composable that displays text with a semi-transparent black background, white color, and bold font weight.
    - `CityCard`: A card composable that displays a city's image and name. It uses `AsyncImage` to load the image and places the city's name on top using the `TextOverlay` composable. The card is clickable to allow for navigation.

---
+++ app/src/main/java/denys/diomaxius/newzealandguide/ui/components/TextOverlay.kt
@@ -0,0 +1,31 @@
+package denys.diomaxius.newzealandguide.ui.components
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+@Composable
+fun TextOverlay(
+    text: String,
+    modifier: Modifier = Modifier
+) {
+    Box(
+        modifier = modifier
+            .background(Color.Black.copy(alpha = 0.3f))
+            .padding(start = 5.dp, end = 6.dp)
+    ) {
+        Text(
+            text = text,
+            fontWeight = FontWeight.SemiBold,
+            fontSize = 36.sp,
+            color = Color.White
+        )
+    }
+}
---
+++ app/src/main/java/denys/diomaxius/newzealandguide/ui/screen/allcities/components/CityCard.kt
@@ -0,0 +1,45 @@
+package denys.diomaxius.newzealandguide.ui.screen.allcities.components
+
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Card
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.unit.dp
+import coil3.compose.AsyncImage
+import denys.diomaxius.newzealandguide.domain.model.city.City
+import denys.diomaxius.newzealandguide.ui.components.TextOverlay
+
+@Composable
+fun CityCard(
+    modifier: Modifier = Modifier,
+    city: City,
+    navigateToCity: () -> Unit
+) {
+    Card(
+        modifier = modifier
+            .padding(bottom = 12.dp)
+            .fillMaxWidth()
+            .clickable {
+                navigateToCity()
+            }
+    ) {
+        Box(
+            contentAlignment = Alignment.BottomStart
+        ) {
+
+            AsyncImage(
+                modifier = Modifier.fillMaxWidth(),
+                model = city.photos.first(),
+                contentDescription = city.name,
+                contentScale = ContentScale.FillWidth
+            )
+
+            TextOverlay(text = city.name)
+        }
+    }
+}
