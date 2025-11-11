f7548a0a3ad172b6434bdf33a314fbc173f95571
Denys <165385842+DoggyDoggyDoggy@users.noreply.github.com>
2025-11-12 12:51:18 +1300

    Refactor: Relocate and enhance CityPlaceEntity
    Moves `CityPlacesEntity` to a new `cityplace` package and updates its structure to establish a proper relationship with `CityEntity`.
    
    This commit includes the following changes:
    - Deletes the old `CityPlacesEntity` from the `...room.model.city` package.
    - Creates the new `CityPlaceEntity` in the `...room.model.cityplace` package.
    - Defines a composite primary key using `cityId` and `name`.
    - Adds a foreign key constraint to link `CityPlaceEntity` with `CityEntity` using `cityId`, with cascading deletes enabled.

---
+++ app/src/main/java/denys/diomaxius/newzealandguide/data/local/room/model/cityplace/CityPlacesEntity.kt
@@ -0,0 +1,26 @@
+package denys.diomaxius.newzealandguide.data.local.room.model.cityplace
+
+import androidx.room.Entity
+import androidx.room.ForeignKey
+import androidx.room.Index
+import denys.diomaxius.newzealandguide.data.local.room.model.city.CityEntity
+
+@Entity(
+    tableName = "city_place",
+    primaryKeys = ["cityId", "name"],
+    foreignKeys = [
+        ForeignKey(
+            entity = CityEntity::class,
+            parentColumns = ["id"],
+            childColumns = ["cityId"],
+            onDelete = ForeignKey.Companion.CASCADE
+        )
+    ],
+    indices = [Index("cityId")]
+)
+data class CityPlaceEntity(
+    val cityId: String,
+    val name: String,
+    val image: String,
+    val description: String
+)
