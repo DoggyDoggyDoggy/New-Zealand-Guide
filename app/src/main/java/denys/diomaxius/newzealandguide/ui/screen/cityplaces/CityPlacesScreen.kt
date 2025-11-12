e76d6cbb3446827adbbb312c600cc6d390597b56
Denys <165385842+DoggyDoggyDoggy@users.noreply.github.com>
2025-11-12 13:21:04 +1300

    feat: Add CityPlaces screen
    Introduces a new screen to display a list of notable places for a specific city.
    
    This commit includes:
    - `CityPlacesScreen`: A new composable that displays a list of attractions in a `LazyColumn`.
    - `CityPlacesScreenViewModel`: A Hilt ViewModel responsible for fetching the places for a given `cityId` using `GetPlacesForCityByIdUseCase`.
    - Individual place details are presented in a styled `Card`, showing the place's name, image, and description.
    - A top app bar with a dynamic title (e.g., "Attractions in [City Name]") and a back navigation button.

---
+++ app/src/main/java/denys/diomaxius/newzealandguide/ui/screen/cityplaces/CityPlacesScreen.kt
@@ -0,0 +1,124 @@
+package denys.diomaxius.newzealandguide.ui.screen.cityplaces
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.hilt.lifecycle.viewmodel.compose.hiltViewModel
+import coil3.compose.AsyncImage
+import denys.diomaxius.newzealandguide.R
+import denys.diomaxius.newzealandguide.domain.model.cityplace.CityPlace
+import denys.diomaxius.newzealandguide.navigation.LocalNavController
+import denys.diomaxius.newzealandguide.ui.components.topbar.PopBackArrowButton
+import denys.diomaxius.newzealandguide.ui.components.topbar.TopBar
+
+@Composable
+fun CityPlacesScreen(
+    viewModel: CityPlacesScreenViewModel = hiltViewModel(),
+    cityName: String,
+) {
+    val cityPlaces by viewModel.cityPlaces.collectAsState()
+
+    val navHostController = LocalNavController.current
+
+    Scaffold(
+        topBar = {
+            TopBar(
+                text = stringResource(R.string.top_bar_attraction_in_city, cityName),
+                navigationButton = {
+                    PopBackArrowButton {
+                        navHostController.navigateUp()
+                    }
+                }
+            )
+        }
+    ) { innerPadding ->
+        Content(
+            modifier = Modifier.padding(innerPadding),
+            cityPlacesTopics = cityPlaces
+        )
+    }
+}
+
+@Composable
+fun Content(
+    modifier: Modifier = Modifier,
+    cityPlacesTopics: List<CityPlace>,
+) {
+    LazyColumn(
+        modifier = modifier
+            .fillMaxSize()
+            .padding(horizontal = 12.dp)
+    ) {
+        items(cityPlacesTopics) { topic ->
+            CityPlace(topic)
+        }
+    }
+}
+
+@Composable
+fun CityPlace(topic: CityPlace) {
+    Card(
+        modifier = Modifier.padding(vertical = 8.dp),
+        shape = RoundedCornerShape(8.dp),colors = CardDefaults.cardColors(
+            containerColor = MaterialTheme.colorScheme.surfaceVariant
+        ),
+        elevation = CardDefaults.cardElevation(6.dp)
+    ) {
+        Column(
+            modifier = Modifier.padding(8.dp)
+        ) {
+            Text(
+                modifier = Modifier.fillMaxWidth(),
+                text = topic.name,
+                fontWeight = FontWeight.Bold,
+                fontSize = 24.sp,
+                textAlign = TextAlign.Center
+            )
+
+            Spacer(
+                modifier = Modifier.height(5.dp)
+            )
+
+            if (topic.image != "") {
+                AsyncImage(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .clip(RoundedCornerShape(8.dp)),
+                    model = topic.image,
+                    contentDescription = null,
+                    contentScale = ContentScale.FillWidth
+                )
+                Spacer(
+                    modifier = Modifier.height(5.dp)
+                )
+            }
+
+            Text(
+                text = topic.description,
+                fontSize = 16.sp
+            )
+        }
+    }
+}
---
+++ app/src/main/java/denys/diomaxius/newzealandguide/ui/screen/cityplaces/CityPlacesScreenViewModel.kt
@@ -0,0 +1,31 @@
+package denys.diomaxius.newzealandguide.ui.screen.cityplaces
+
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import dagger.hilt.android.lifecycle.HiltViewModel
+import denys.diomaxius.newzealandguide.domain.model.cityplace.CityPlace
+import denys.diomaxius.newzealandguide.domain.usecase.cityplaces.GetPlacesForCityByIdUseCase
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class CityPlacesScreenViewModel @Inject constructor(
+    private val getPlacesForCityByIdUseCase: GetPlacesForCityByIdUseCase,
+    savedStateHandle: SavedStateHandle,
+) : ViewModel() {
+    private val _cityPlaces = MutableStateFlow<List<CityPlace>>(emptyList())
+    val cityPlaces = _cityPlaces.asStateFlow()
+
+    private val cityId: String = checkNotNull(savedStateHandle["cityId"])
+
+    init {
+        loadPlaces()
+    }
+
+    private fun loadPlaces() = viewModelScope.launch {
+        _cityPlaces.value = getPlacesForCityByIdUseCase(cityId)
+    }
+}
