bc62f89e36f21f839e96bc0afe9e53e08beddec0
Denys <165385842+DoggyDoggyDoggy@users.noreply.github.com>
2025-11-19 12:42:14 +1300

    feat: Add foreign key to WeatherCacheInfo
    Adds a foreign key relationship from `WeatherCacheInfo` to `CityEntity`.
    
    This change ensures that when a `CityEntity` is deleted, its corresponding `WeatherCacheInfo` entry is also removed from the database, maintaining data integrity. An index is also added on the `cityId` column to improve query performance.

--- app/src/main/java/denys/diomaxius/newzealandguide/data/local/room/model/cache/WeatherCacheInfo.kt
+++ app/src/main/java/denys/diomaxius/newzealandguide/data/local/room/model/cache/WeatherCacheInfo.kt
@@ -1,10 +1,25 @@
 package denys.diomaxius.newzealandguide.data.local.room.model.cache
 
 import androidx.room.Entity
+import androidx.room.ForeignKey
+import androidx.room.Index
 import androidx.room.PrimaryKey
+import denys.diomaxius.newzealandguide.data.local.room.model.city.CityEntity
 import java.time.Instant
 
-@Entity(tableName = "weather_cache_info")
+@Entity(
+    tableName = "weather_cache_info",
+    foreignKeys = [
+        ForeignKey(
+            entity = CityEntity::class,
+            parentColumns = ["id"],
+            childColumns = ["cityId"],
+            onDelete = ForeignKey.Companion.CASCADE
+        )
+    ],
+    indices = [Index("cityId")]
+)
+
 data class WeatherCacheInfo(
     @PrimaryKey val cityId: String,
     val lastSyncedTimestamp: Instant
