055f05dba0cc647a677bfd46b51a537ca3f56851
Denys <165385842+DoggyDoggyDoggy@users.noreply.github.com>
2025-11-19 15:00:27 +1300

    Refactor: Remove unused PrimaryKey import from CityWeatherEntity
    Removes the unnecessary `androidx.room.PrimaryKey` import from the `CityWeatherEntity` data class.

--- app/src/main/java/denys/diomaxius/newzealandguide/data/local/room/model/city/CityWeatherEntity.kt
+++ app/src/main/java/denys/diomaxius/newzealandguide/data/local/room/model/city/CityWeatherEntity.kt
@@ -3,7 +3,6 @@ package denys.diomaxius.newzealandguide.data.local.room.model.city
 import androidx.room.Entity
 import androidx.room.ForeignKey
 import androidx.room.Index
-import androidx.room.PrimaryKey
 
 @Entity(
     tableName = "city_weather",
