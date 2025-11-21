adf34c1b5f8b03bb6e570e42afe0bf9de91621cb
Denys <165385842+DoggyDoggyDoggy@users.noreply.github.com>
2025-11-22 10:09:25 +1300

    feat: Add CityEventEntity to Room database
    Introduces the `CityEventEntity` to the list of entities managed by the `CityDatabase`. This change prepares the database schema to store city-related event information.

--- app/src/main/java/denys/diomaxius/newzealandguide/data/local/room/database/CityDatabase.kt
+++ app/src/main/java/denys/diomaxius/newzealandguide/data/local/room/database/CityDatabase.kt
@@ -7,6 +7,7 @@ import denys.diomaxius.newzealandguide.data.local.room.converter.Converters
 import denys.diomaxius.newzealandguide.data.local.room.dao.CityDao
 import denys.diomaxius.newzealandguide.data.local.room.model.cache.WeatherCacheInfo
 import denys.diomaxius.newzealandguide.data.local.room.model.city.CityEntity
+import denys.diomaxius.newzealandguide.data.local.room.model.city.CityEventEntity
 import denys.diomaxius.newzealandguide.data.local.room.model.city.CityHistoryEntity
 import denys.diomaxius.newzealandguide.data.local.room.model.city.CityPlaceEntity
 import denys.diomaxius.newzealandguide.data.local.room.model.city.CityWeatherEntity
@@ -17,12 +18,13 @@ import denys.diomaxius.newzealandguide.data.local.room.model.city.CityWeatherEnt
         CityHistoryEntity::class,
         CityPlaceEntity::class,
         WeatherCacheInfo::class,
-        CityWeatherEntity::class
+        CityWeatherEntity::class,
+        CityEventEntity::class
     ],
     version = 1
 )
 @TypeConverters(Converters::class)
-abstract class CityDatabase: RoomDatabase() {
+abstract class CityDatabase : RoomDatabase() {
     companion object {
         const val DATABASE_NAME = "city_db"
     }
