9692a75f2faa4ae364ed84a78510967ed1b5f9cd
Denys <165385842+DoggyDoggyDoggy@users.noreply.github.com>
2025-11-22 10:12:24 +1300

    feat: Add domain layer for city events
    Introduces the domain layer components for handling city events.
    
    This includes:
    - `CityEvent`: A new data class to model a city event, containing details such as ID, name, description, address, image URL, and a list of session strings.
    - `CityEventsRepository`: An interface defining a contract for fetching paginated city event data via a `PagingData` Flow.

---
+++ app/src/main/java/denys/diomaxius/newzealandguide/domain/model/city/CityEvent.kt
@@ -0,0 +1,12 @@
+package denys.diomaxius.newzealandguide.domain.model.city
+
+data class CityEvent(
+    val cityId: String,
+    val eventId: String,
+    val url: String,
+    val name: String,
+    val description: String,
+    val address: String,
+    val imageUrl: String,
+    val sessions: List<String>
+)
---
+++ app/src/main/java/denys/diomaxius/newzealandguide/domain/repository/city/CityEventsRepository.kt
@@ -0,0 +1,9 @@
+package denys.diomaxius.newzealandguide.domain.repository.city
+
+import androidx.paging.PagingData
+import denys.diomaxius.newzealandguide.domain.model.city.CityEvent
+import kotlinx.coroutines.flow.Flow
+
+interface CityEventsRepository {
+    fun cityEventsPagerFlow(pageSize: Int): Flow<PagingData<CityEvent>>
+}
