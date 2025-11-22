af105ee55a8e87307751a68ec5eb6bd503307f93
Denys <165385842+DoggyDoggyDoggy@users.noreply.github.com>
2025-11-22 19:42:44 +1300

    feat: Add skeleton for CityEventsRemoteMediator
    Introduces the initial structure for `CityEventsRemoteMediator`, which will handle pagination for city events.
    
    This commit adds:
    - A new `CityEventsRemoteMediator` class that inherits from `RemoteMediator<Int, CityEventEntity>`.
    - A placeholder implementation for the `load` function.

---
+++ app/src/main/java/denys/diomaxius/newzealandguide/data/remote/paging/CityEventsRemoteMediator.kt
@@ -0,0 +1,21 @@
+package denys.diomaxius.newzealandguide.data.remote.paging
+
+import androidx.paging.ExperimentalPagingApi
+import androidx.paging.LoadType
+import androidx.paging.PagingState
+import androidx.paging.RemoteMediator
+import denys.diomaxius.newzealandguide.data.local.room.model.city.CityEventEntity
+
+@OptIn(ExperimentalPagingApi::class)
+class CityEventsRemoteMediator(
+    private val pageSize: Int,
+) : RemoteMediator<Int, CityEventEntity>() {
+    override suspend fun load(
+        loadType: LoadType,
+        state: PagingState<Int, CityEventEntity>,
+    ): MediatorResult {
+        val limit = state.config.pageSize.takeIf { it > 0 } ?: pageSize
+        TODO("Not yet implemented")
+    }
+
+}
