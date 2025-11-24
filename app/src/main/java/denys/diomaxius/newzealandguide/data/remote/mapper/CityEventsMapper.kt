b90b32dd5bbca623cfcb734cc6b4b69bbd0673c7
Denys <165385842+DoggyDoggyDoggy@users.noreply.github.com>
2025-11-24 17:10:06 +1300

    feat: Add mapper for CityEventDto to CityEventEntity
    Introduces an extension function `toEntity()` to map a `CityEventDto` from the remote data layer to a `CityEventEntity` for the local database.

---
+++ app/src/main/java/denys/diomaxius/newzealandguide/data/remote/mapper/CityEventsMapper.kt
@@ -0,0 +1,16 @@
+package denys.diomaxius.newzealandguide.data.remote.mapper
+
+import denys.diomaxius.newzealandguide.data.local.room.model.city.CityEventEntity
+import denys.diomaxius.newzealandguide.data.remote.model.CityEventDto
+
+fun CityEventDto.toEntity(): CityEventEntity =
+    CityEventEntity(
+        cityId = cityId,
+        eventId = eventId,
+        url = url,
+        name = name,
+        description = description,
+        address = address,
+        imageUrl = imageUrl,
+        sessions = sessions
+    )
