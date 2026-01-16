92fd85f16214aadaba3d50c15239013091ee92bc
Denys <165385842+DoggyDoggyDoggy@users.noreply.github.com>
2026-01-17 08:40:34 +1300

    feat: Enhance EventDetailsScreen UI
    Improves the `EventDetailsScreen` by replacing the placeholder event name with a detailed layout.
    
    This commit introduces the following changes:
    - Wraps the event details within a `Card` composable for better visual structure.
    - Displays the event name, image, description, and address.
    - Creates two new reusable composables:
        - `EventDescription`: For displaying the event's description with a title.
        - `EventAddress`: For showing the event's location with a location icon.
    - Replaces the `BuyTicketButton` with `EventBottomBar` in the `EventDetailsScreen` Scaffold.

--- app/src/main/java/denys/diomaxius/newzealandguide/ui/screen/event/EventDetailsScreen.kt
+++ app/src/main/java/denys/diomaxius/newzealandguide/ui/screen/event/EventDetailsScreen.kt
@@ -1,29 +1,42 @@
 package denys.diomaxius.newzealandguide.ui.screen.event
 
 import android.content.Context
-import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.asPaddingValues
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.navigationBars
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.hilt.lifecycle.viewmodel.compose.hiltViewModel
+import coil3.compose.AsyncImage
 import denys.diomaxius.newzealandguide.domain.model.city.CityEvent
 import denys.diomaxius.newzealandguide.navigation.LocalNavController
 import denys.diomaxius.newzealandguide.ui.components.topbar.PopBackArrowButton
 import denys.diomaxius.newzealandguide.ui.components.topbar.TopBar
 import denys.diomaxius.newzealandguide.ui.components.uistate.UiStateHandler
-import denys.diomaxius.newzealandguide.ui.screen.event.components.BuyTicketButton
+import denys.diomaxius.newzealandguide.ui.screen.event.components.EventAddress
 import denys.diomaxius.newzealandguide.ui.screen.event.components.EventBottomBar
+import denys.diomaxius.newzealandguide.ui.screen.event.components.EventDescription
 
 @Composable
 fun EventDetailsScreen(
@@ -68,5 +81,48 @@ fun Content(
     modifier: Modifier,
     event: CityEvent,
 ) {
-    Text(event.name)
+    Card(
+        modifier = modifier
+            .padding(top = 12.dp)
+            .padding(horizontal = 12.dp),
+        colors = CardDefaults.cardColors(
+            containerColor = MaterialTheme.colorScheme.surfaceVariant
+        ),
+        elevation = CardDefaults.cardElevation(6.dp)
+    ) {
+        Column(
+            modifier = Modifier
+                .padding(horizontal = 12.dp)
+                .verticalScroll(rememberScrollState())
+        ) {
+            Spacer(modifier = Modifier.height(12.dp))
+
+            Text(
+                text = event.name,
+                fontWeight = FontWeight.Bold,
+                fontSize = 24.sp
+            )
+
+            Spacer(modifier = Modifier.height(6.dp))
+
+            AsyncImage(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .clip(RoundedCornerShape(8.dp)),
+                model = event.imageUrl,
+                contentScale = ContentScale.FillWidth,
+                contentDescription = "Image"
+            )
+
+            Spacer(modifier = Modifier.height(6.dp))
+
+            EventDescription(event.description)
+
+            Spacer(modifier = Modifier.height(12.dp))
+
+            EventAddress(event.address)
+
+            Spacer(modifier = Modifier.height(4.dp))
+        }
+    }
 }
