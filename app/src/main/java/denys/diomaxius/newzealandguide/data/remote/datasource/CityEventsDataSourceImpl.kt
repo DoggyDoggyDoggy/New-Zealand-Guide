da83b199b4a351e337e394f07774c2e3b75d715b
Denys <165385842+DoggyDoggyDoggy@users.noreply.github.com>
2026-01-31 15:54:30 +1300

    Fix: Force Firestore data fetch from server
    Forces the `getCityEvents` data source to fetch data directly from the server, bypassing the local cache.
    
    This is achieved by changing the Firestore query from `.get()` to `.get(Source.SERVER)`, ensuring that the application always displays the most up-to-date event information.

--- app/src/main/java/denys/diomaxius/newzealandguide/data/remote/datasource/CityEventsDataSourceImpl.kt
+++ app/src/main/java/denys/diomaxius/newzealandguide/data/remote/datasource/CityEventsDataSourceImpl.kt
@@ -3,6 +3,7 @@ package denys.diomaxius.newzealandguide.data.remote.datasource
 import android.util.Log
 import com.google.firebase.firestore.FieldPath
 import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.Source
 import denys.diomaxius.newzealandguide.data.remote.api.CityEventsDataSource
 import denys.diomaxius.newzealandguide.data.remote.model.CityEventDto
 import kotlinx.coroutines.tasks.await
@@ -32,7 +33,7 @@ class CityEventsDataSourceImpl @Inject constructor(
             query = query.startAfter(lastDocId)
         }
 
-        return query.get().await().mapNotNull{
+        return query.get(Source.SERVER).await().mapNotNull{
             it.toObject(CityEventDto::class.java)
         }
     }
