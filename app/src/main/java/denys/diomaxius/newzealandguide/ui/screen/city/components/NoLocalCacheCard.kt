3f362400911e84288c31ae1a4e3d57755896b429
Denys <165385842+DoggyDoggyDoggy@users.noreply.github.com>
2026-02-03 16:52:43 +1300

    style: Enhance `NoLocalCacheCard` content
    Improves the user-facing message on the `NoLocalCacheCard` to be more descriptive and user-friendly. The text is now clearer about why the screen is empty and what action the user can take.
    
    - Updates the main heading to "It's empty here for now".
    - Adds a descriptive subtext: "To find out the weather forecast and a list of upcoming events, connect to the internet."
    - Adjusts styling for better readability, including font size, weight, text alignment, and padding.
    - Adds vertical spacing between the heading and the subtext using a `Spacer`.

--- app/src/main/java/denys/diomaxius/newzealandguide/ui/screen/city/components/NoLocalCacheCard.kt
+++ app/src/main/java/denys/diomaxius/newzealandguide/ui/screen/city/components/NoLocalCacheCard.kt
@@ -2,15 +2,20 @@ package denys.diomaxius.newzealandguide.ui.screen.city.components
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Card
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 @Composable
 fun NoLocalCacheCard(
@@ -22,15 +27,25 @@ fun NoLocalCacheCard(
             .fillMaxWidth()
     ) {
         Column(
-            modifier = Modifier.fillMaxSize(),
+            modifier = Modifier.fillMaxSize().padding(12.dp),
             horizontalAlignment = Alignment.CenterHorizontally,
             verticalArrangement = Arrangement.Center
         ) {
             Text(
-                text = "No local cache"
+                modifier = Modifier.fillMaxWidth(),
+                text = "It's empty here for now",
+                fontSize = 24.sp,
+                textAlign = TextAlign.Center,
+                fontWeight = FontWeight.Bold
             )
+
+            Spacer(modifier = Modifier.height(12.dp))
+
             Text(
-                text = "Check your internet connection"
+                modifier = Modifier.fillMaxWidth(),
+                text = "To find out the weather forecast and a list of upcoming events, connect to the internet.",
+                fontSize = 16.sp,
+                textAlign = TextAlign.Center,
             )
         }
     }
