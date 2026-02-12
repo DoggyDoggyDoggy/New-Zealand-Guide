eeaa0d67fea6047700d700e5d037acf829d0d78a
Denys <165385842+DoggyDoggyDoggy@users.noreply.github.com>
2026-02-12 18:25:37 +1300

    feat: Enable scrolling on Maori Hub screen
    Makes the content of the `MaoriHubScreen` scrollable by adding a `verticalScroll` modifier to the root `Column`.
    
    This commit also adjusts the layout by removing the `weight(1f)` modifier from the `NavigationCard` composables and setting a fixed height of `110.dp` instead. This prevents the cards from improperly expanding to fill available space, ensuring a consistent layout when the screen content needs to scroll.

--- app/src/main/java/denys/diomaxius/newzealandguide/ui/screen/maorihubscreen/MaoriHubScreen.kt
+++ app/src/main/java/denys/diomaxius/newzealandguide/ui/screen/maorihubscreen/MaoriHubScreen.kt
@@ -5,6 +5,8 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
@@ -68,7 +70,9 @@ fun Content(
     val horizontalPadding = 16.dp
 
     Column(
-        modifier = modifier.fillMaxSize(),
+        modifier = modifier
+            .fillMaxSize()
+            .verticalScroll(rememberScrollState()),
         verticalArrangement = Arrangement.spacedBy(16.dp)
     ) {
         HeroBlock(
@@ -85,7 +89,7 @@ fun Content(
         NavigationCard(
             modifier = Modifier
                 .padding(horizontal = horizontalPadding)
-                .weight(1f),
+                .height(110.dp),
             name = "Kupu",
             description = "Common maori words",
             onClick = {
@@ -99,7 +103,7 @@ fun Content(
         NavigationCard(
             modifier = Modifier
                 .padding(horizontal = horizontalPadding)
-                .weight(1f),
+                .height(110.dp),
             name = "Learning resources",
             description = "Articles & Links",
             onClick = {},
