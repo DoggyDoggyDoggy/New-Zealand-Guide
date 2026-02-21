9526c4ce9fd21b571a9957c7581520144fa66a51
Denys <165385842+DoggyDoggyDoggy@users.noreply.github.com>
2026-02-21 15:31:51 +1300

    Refactor: Simplify WeatherResult sealed class
    Removes the `NoInternetAndNoCache` state from the `WeatherResult` sealed class. This state is now considered redundant, as its purpose can be handled by the existing `Error` state.

--- app/src/main/java/denys/diomaxius/newzealandguide/domain/model/city/WeatherResult.kt
+++ app/src/main/java/denys/diomaxius/newzealandguide/domain/model/city/WeatherResult.kt
@@ -2,6 +2,5 @@ package denys.diomaxius.newzealandguide.domain.model.city
 
 sealed class WeatherResult {
     data class Success(val data: List<CityWeather>) : WeatherResult()
-    object NoInternetAndNoCache : WeatherResult()
     data class Error(val exception: Throwable) : WeatherResult()
 }
