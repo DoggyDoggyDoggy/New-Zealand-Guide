7205d70107e67b1b1d05c115824afc9a624a2ed0
Denys <165385842+DoggyDoggyDoggy@users.noreply.github.com>
2026-02-22 18:53:24 +1300

    chore: Bump version code and name
    Increases the application version for a new release.
    
    - `versionCode` updated from 9 to 10.
    - `versionName` updated from "3.1" to "3.2".

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "denys.diomaxius.newzealandguide"
         minSdk = 26
         targetSdk = 36
-        versionCode = 9
-        versionName = "3.1"
+        versionCode = 10
+        versionName = "3.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
