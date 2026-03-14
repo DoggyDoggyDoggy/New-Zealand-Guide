6ef0afd8f273d34a79c123473e2d9c832b30ffdc
Denys <165385842+DoggyDoggyDoggy@users.noreply.github.com>
2026-03-15 04:06:30 +1300

    refactor: Add debug logging to `IncrementReviewActionUseCase`
    Introduces a debug log statement in `IncrementReviewActionUseCase` to track when the review action count is incremented.
    
    - **`IncrementReviewActionUseCase.kt`**:
        - Adds `Log.d` to the `invoke` function to log the "IncrementReviewAction" event.

--- app/src/main/java/denys/diomaxius/newzealandguide/feature_review/usecase/IncrementReviewActionUseCase.kt
+++ app/src/main/java/denys/diomaxius/newzealandguide/feature_review/usecase/IncrementReviewActionUseCase.kt
@@ -1,5 +1,6 @@
 package denys.diomaxius.newzealandguide.feature_review.usecase
 
+import android.util.Log
 import denys.diomaxius.newzealandguide.feature_review.data.ReviewPreferencesManager
 import javax.inject.Inject
 
@@ -7,6 +8,7 @@ class IncrementReviewActionUseCase @Inject constructor(
     private val repository: ReviewPreferencesManager
 ) {
     suspend operator fun invoke() {
+        Log.d("feature_review","IncrementReviewAction")
         repository.incrementActionCount()
     }
 }
