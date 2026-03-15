252472316a00458d454ceb1d823a2efad5408730
Denys <165385842+DoggyDoggyDoggy@users.noreply.github.com>
2026-03-16 09:33:32 +1300

    chore: Update application version to 3.9
    Increments the version code and name in the build configuration.
    
    - **`app/build.gradle.kts`**:
        - Updated `versionCode` to 17.
        - Updated `versionName` to "3.9".

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "denys.diomaxius.newzealandguide"
         minSdk = 26
         targetSdk = 36
-        versionCode = 16
-        versionName = "3.8"
+        versionCode = 17
+        versionName = "3.9"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
